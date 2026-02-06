b546a595bcdbdeb969da768f22af113619ea3a9c
Ariana <136492482+aryins@users.noreply.github.com>
2026-02-06 20:13:59 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../styles/Header.css";
+import "../styles/header.css";
 import { useAuth } from "../context/AuthContext.jsx";
 
 
@@ -25,4 +25,5 @@ const { logout, user } = useAuth();
   );
 }
  
+
  export { Header };
