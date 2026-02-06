6da35ee3aec99d4a7bcae6d472cf9ef27be7ee4e
Ariana <136492482+aryins@users.noreply.github.com>
2026-02-06 20:04:51 -0300

    Update QuienesSomos.jsx

--- src/views/QuienesSomos.jsx
+++ src/views/QuienesSomos.jsx
@@ -1,6 +1,6 @@
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
-import "../styles/QuienesSomos.css" 
+import "../styles/quienesSomos.css" 
 
 
 const QuienesSomos = () => {
@@ -74,3 +74,4 @@ const QuienesSomos = () => {
 
 
 export { QuienesSomos }
+
